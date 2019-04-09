91a7f77ac7526e8807db8209011b64185ee836c0
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-09 18:14:37 -0400

    Successfully tested foul function and ball function

--- webtestingii/src/App.test.js
+++ webtestingii/src/App.test.js
@@ -15,10 +15,41 @@ describe("<App />", () => {
     render(<App />);
   });
 
-  it("Successfully strikes", () => {
+  it("successfully strikes", () => {
     const { getAllByText, getByText } = render(<App />);
     const button = getAllByText(/strike/i)[1];
     fireEvent.click(button);
     getByText(/1/);
+  });
+  it("successfully resets strikes", () => {
+    const { getAllByText, getByText } = render(<App />);
+    const button = getAllByText(/strike/i)[1];
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    getByText(/0/);
+  });
+  it("successfully balls", () => {
+    const { getAllByText, getByText } = render(<App />);
+    const button = getAllByText(/ball/i)[1];
+    fireEvent.click(button);
+    getByText(/1/);
+  });
+  it("successfully resets balls", () => {
+    const { getAllByText, getByText } = render(<App />);
+    const button = getAllByText(/ball/i)[1];
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    getAllByText(/0/)[1];
+  });
+  it("successfully fouls", () => {
+    const { getByText } = render(<App />);
+    const button = getByText(/foul/i);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    getByText(/2/);
   });
 });
