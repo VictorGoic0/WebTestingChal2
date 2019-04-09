8a04be215bf66c56c1186ce291b13cca6bf4bae6
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-09 17:32:22 -0400

    added methods to App for foulBall, ball, or strike

--- webtestingii/src/App.js
+++ webtestingii/src/App.js
@@ -4,6 +4,31 @@ import Dashboard from "./Dashboard";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    strikes: 0,
+    balls: 0
+  };
+
+  foulBall = () => {
+    this.setState(prevState => ({
+      strikes: prevState.strikes < 2 ? prevState.strikes + 1 : prevState.strikes
+    }));
+  };
+
+  ball = () => {
+    this.setState(prevState => ({
+      balls: prevState.balls === 3 ? 0 : prevState.balls + 1,
+      strikes: prevState.balls === 3 ? 0 : prevState.strikes
+    }));
+  };
+
+  strike = () => {
+    this.setState(prevState => ({
+      balls: prevState.strikes === 2 ? 0 : prevState.balls,
+      strikes: prevState.strikes === 2 ? 0 : prevState.strikes + 1
+    }));
+  };
+
   render() {
     return (
       <div className="App">
